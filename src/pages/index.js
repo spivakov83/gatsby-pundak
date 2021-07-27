3c7a1f78b36a9c7dd7a03af3d4a59427bca10f56
spivapp2@gmail.com <8JwdL4vM>
2021-07-27 09:21:07 +0300

    redirect

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,52 +1,26 @@
-import React from "react"
-
-import Layout from "../components/layout"
-import SEO from "../components/seo"
+import React, {useEffect} from "react"
 import {graphql} from 'gatsby'
-import Img from 'gatsby-image'
-import Menu from '../components/menu/menu'
-import Instagram from '../components/instagram'
-import LeafletMap from '../components/leafletMap'
-import Footer from '../components/footer'
-import WhatsappBtn from "../components/WhatsappBtn/whatsapp-button"
 
-const IndexPage = ({data}) => {
 
+const IndexPage = () => {
+  useEffect(() => {
+    window.location.href = "https://hapunjack.com";
+  })
   return (
-    <Layout>
-      <SEO title="הפונג'ק" />
-      <section className="hero">
-        <Img fluid={data.coffeeShop.childImageSharp.fluid} className="hero-image" />
-      </section>
-      <section className="container">
-        <span className="description">
-          {data.strapiHomepage.topText}
-        </span>
-        <div className="gallery-grid">
-          <Img fluid={data.logo.childImageSharp.fluid} className="gallery-img1" />
-          <Img fluid={data.coffeeBags.childImageSharp.fluid} className="gallery-img2" />
-          <Img fluid={data.coffeePortrait.childImageSharp.fluid} className="gallery-img3" />
+    <>
+      <div className="moved-container">
+        <div>
+          <h1>
+            מיד תועברו לאתר החדש...
+          </h1>
         </div>
-        <div className="about-grid">
-          <h2> {data.strapiHomepage.mainTextTitle}</h2>
-          <p>{data.strapiHomepage.mainTextContent}</p>
-        </div>
-      </section>
-      <div className="parallax"></div>
-      <section className="container">
-        <Menu />
-        <Instagram />
-      </section>
-      {typeof window !== 'undefined' &&
-        <LeafletMap
-          position={[31.982976, 34.953053]} // Your Coordinates
-          zoom={18} // Zoom Level
-          markerText={"הפונדק של ג'ק. בית נחמיה"} // Icon text
-        />
-      }
-      <Footer />
-      <WhatsappBtn/>
-    </Layout>
+      </div>
+      <div className="moved-link">
+        <a href="https://hapujack.com">
+          לאתר החדש
+        </a>
+      </div>
+    </>
   )
 }
 
